a30b06b7a571f9bc9aee08ae75093e890cd5d7b5
vybhavb <vybhavbugs+github@gmail.com>
2019-04-23 14:29:26 -0700

    Updating website to have a better ui for projects

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -2,35 +2,45 @@
 <template>
   <div class="projects">
 
-    <section class="hero is-fullheight-with-navbar">
-      <div class="hero-heas">
+    <section class="hero  has-background-primary is-fullheight-with-navbar">
+      <div class="hero-head">
         <div class="container">
-            <table class="table">
-              <tbody>
-                <tr v-for="proj in this.projects" :key="proj.index">
-                  <th class="is-size-3 is-size-5-mobile">{{proj.name}}</th>
-                  <td class="is-size-5 is-size-7-mobile">{{proj.description}}</td>
-                  <td>
-                    <div class="tags">
+          <h1 id="projTitle" class="is-size-2 is-size-4-mobile title has-text-white has-text-centered">heres some of my projects</h1>
+        </div>
+      </div>
+      <div class="hero-body">
+        <div>
+          <div class="columns box" v-for="proj in projects" v-bind:key="proj.index">
+            <div class="column is-one-quarter">
+              <h1 class="is-size-3 is-size-5-mobile">
+                <a :href="proj.linkToSite || proj.linkToSource" class="has-text-primary">
+                  {{proj.name}}
+                </a>
+              </h1>
+            </div>
+            <div class="column is-half">
+              <h1 class="is-size-5 is-size-7-mobile">
+                {{proj.description}}
+              </h1>
+            </div>
+            <div class="column" v-if="proj.technologies">
+              <div class="tags">
                       <span v-for="tech in proj.technologies" class="tag" :class="tech.color">{{tech.name}}</span>
-                    </div>
-                  </td>
-                  <td>
-                    <a class="button" :href="proj.linkToSource">
-                      <font-awesome-icon :icon="{ prefix: 'fab', iconName: 'github'}">
-                        </font-awesome-icon>
-                    </a>
-                  </td>
-                  <td>
-                    <a class="button" v-if="proj.linkToSite != ''" :href="proj.linkToSite">
-                      <font-awesome-icon icon="globe">
-                        </font-awesome-icon>
-                    </a>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
+              </div>
+            </div>
+            <div class="column is-narrow" v-if="proj.technologies">
+              <div class="buttons are-large">
+                <a class="button" v-if="proj.linkToSource" :href="proj.linkToSource">
+                  <font-awesome-icon :icon="{ prefix: 'fab', iconName: 'github'}">
+                  </font-awesome-icon>
+                </a>
+                <a class="button" v-if="proj.linkToSite" :href="proj.linkToSite">
+                  <font-awesome-icon icon="globe"></font-awesome-icon>
+                </a>
+              </div>
+            </div>
           </div>
+        </div>
       </div>
       <div class="hero-foot">
         <nav class="is-fullwidth">
@@ -38,7 +48,6 @@
         </nav>
       </div>
     </section>
-
   </div>
 </template>
 
