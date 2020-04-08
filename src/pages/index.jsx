d87dc450055fc3d010bef4af3dac4812347c5b85
Vybhav Achar Bhargav <contact@vybhavb.com>
2020-04-08 13:17:26 -0700

    testing out new setup for the index page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-// import Typed from 'react-typed';
+import Typed from 'react-typed';
 import classnames from 'classnames';
 import { connect } from 'react-redux';
 // import PropTypes from 'prop-types';
@@ -27,16 +27,13 @@ class Index extends React.Component {
         <section className="hero is-dark is-large">
           <div className="hero-body">
             <div className="container">
-              <h1 className={classnames('title', 'is-size-2', { 'fade-in': shouldDoFade })} onAnimationEnd={this.callFirst}>
-                Hey! I&apos;m Vybhav.
+              <h1 className={classnames('title', 'is-2', 'is-3-mobile', { 'fade-in': shouldDoFade })} onAnimationEnd={this.callFirst}>
+                Hey I&apos;m Vybhav!
               </h1>
-              <p className="subtitle">
-                tl;dr:&nbsp;
-                {/* <Typed
-              strings={['i am you', 'you am i']}
-              typeSpeed={40}
-              backSpeed={20}
-            /> */}
+              <p className={classnames('subtitle', 'is-5', { 'fade-in': shouldDoFade })}>
+                tl;dr:&nbsp; <br />
+                I build scaleable, fast and secure applications. Check some of them out over at <a href="/projects" className="has-text-link">/projects</a> <br />
+                Want to chat? Email me → <a href="mailto:contact@vybhavb.com" className="has-text-link">contact@vybhavb.com</a> or find me anywhere @<a href="https://keybase.io/vybhavb" className="has-text-link">vybhavb</a>
               </p>
             </div>
           </div>
